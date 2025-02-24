bfc0c509840f943e26a58b0130f9523e4dddd6b7
Khusamov Sukhrob <khusamov@yandex.ru>
2025-02-24 22:22:11 +0300

    Добавлена заготовка с использованием пакета octokit

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ npx khusamov-backup
 
 Скрипт будет создан на основе кода:  
 https://github.com/khusamov/khusamov.github.io/blob/main/scripts/backup-repo.mjs
+
+Файл .env
+---------
+
+В корне добавьте файл `.env` со следующим содержимым:
+
+```
+TOKEN = <Сюда добавьте токен Fine-grained tokens>
+```
+
+Токен следует сгенерировать на странице:
+https://github.com/settings/personal-access-tokens/
--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 #! /usr/bin/env node
 
-console.log('khusamov-backup')
+import {config} from 'dotenv'
+import {Octokit} from 'octokit'
+import packageInformation from './package.json' with {type: 'json'}
+const {description} = packageInformation
+
+config()
+console.log(description)
+
+
+// https://github.com/settings/personal-access-tokens/5367542
+const token = process.env.TOKEN
+
+// https://docs.github.com/rest'
+const octokit = new Octokit({auth: token})
+
+const userInformation = await getUserInformation()
+const userRepositories = await getUserRepositories()
+
+console.log(`Имя пользователя: ${userInformation.name} (${userInformation.login})`)
+console.log(`Публичных репозиториев: ${userInformation.public_repos}`)
+console.log(`Публичных идей (gists): ${userInformation.public_gists}`)
+console.log('result', userRepositories.map(item => item.name + ' ' + item.owner.login))
+console.log('length', userRepositories.length)
+
+async function getUserRepositories() {
+    const result = (
+        // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-the-authenticated-user
+        await octokit.request('GET /user/repos', {
+            visibility: 'all',
+            per_page: 200,
+            affiliation: 'owner'
+        })
+    )
+    return result.data
+}
+
+async function getUserInformation() {
+    const result = (
+        await octokit.request('GET /user', {
+
+        })
+    )
+    return result.data
+}
